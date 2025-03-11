9dc72e28202b736ce5eaf74282e45f60acd386de
Palak Manchanda <apalak910@gmail.com>
2025-03-11 17:49:48 +0530

    Solution to Find Student with Minimum Marks

---
+++ Find Student with Minimum Marks@@67877d61dc107486ad5c2749/code.c
@@ -0,0 +1,16 @@
+// Your code here...
+#include <stdio.h>
+struct student{
+    int rollno;
+    char name[100];
+    float marks;
+};
+int main(){
+    int n;
+    scanf("%d",&n);
+    struct student s[n],min_marks;
+    for(int i=0;i<n;i++){
+        scanf("%d"%s %f,&s[i].name, &s[i].marks);
+        if()
+    }
+}
