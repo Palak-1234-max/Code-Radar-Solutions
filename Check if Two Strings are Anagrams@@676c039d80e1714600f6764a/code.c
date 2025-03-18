9ebcddacf9664cdbf6fe97f21131984c0b810f75
Palak Manchanda <apalak910@gmail.com>
2025-03-18 23:51:10 +0530

    Solution to Check if Two Strings are Anagrams

--- Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
+++ Check if Two Strings are Anagrams@@676c039d80e1714600f6764a/code.c
@@ -1,30 +1,40 @@
 // Your code here...
 #include <stdio.h>
-#include <string.h>
-int main(){
-    char a[50],b[50];
-    int i,j,flag;
+
+#define MAX_CHAR 256  // Total number of characters (ASCII range)
+
+int main() {
+    char a[50], b[50];
+    int freqA[MAX_CHAR] = {0}, freqB[MAX_CHAR] = {0};
+    int i;
+
+    // Read two strings
     fgets(a, sizeof(a), stdin);
     fgets(b, sizeof(b), stdin);
+
+    // Remove newline character if present (from fgets)
     a[strcspn(a, "\n")] = '\0';
     b[strcspn(b, "\n")] = '\0';
-    for(i=0;a[i]!='\0';i++){
-        flag=0;
-        for(j=0;b[j]!='\0';j++){
-            if(a[i]==b[j]){
-                flag=1;
-                break;
-            }
+
+    // Count frequency of each character in string a
+    for(i = 0; a[i] != '\0'; i++) {
+        freqA[(int)a[i]]++;
+    }
+
+    // Count frequency of each character in string b
+    for(i = 0; b[i] != '\0'; i++) {
+        freqB[(int)b[i]]++;
+    }
+
+    // Compare frequency arrays of both strings
+    for(i = 0; i < MAX_CHAR; i++) {
+        if(freqA[i] != freqB[i]) {
+            printf("No\n");
+            return 0;  // If frequencies don't match, no need to check further
         }
-        if(flag==1){
-            break;
-        }
     }
-    if(flag==1){
-        printf("Yes");
-    }
-    else{
-        printf("No");
-    }
+
+    // If frequencies match, print "Yes"
+    printf("Yes\n");
     return 0;
 }
