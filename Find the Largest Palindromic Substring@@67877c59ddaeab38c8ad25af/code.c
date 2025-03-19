0f20e5850151c9ee962733b92eeb588d4ad3aa26
Palak Manchanda <apalak910@gmail.com>
2025-03-19 18:45:16 +0530

    Solution to Find the Largest Palindromic Substring

--- Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
+++ Find the Largest Palindromic Substring@@67877c59ddaeab38c8ad25af/code.c
@@ -3,60 +3,56 @@
 #include <string.h>
 
 // Helper function to expand around the center and find the longest palindrome
-char *expandAroundCenter(char str[], int left, int right, int length) {
+void expandAroundCenter(char str[], int left, int right, int length, int *start, int *maxLen) {
     // Expand as long as the characters at both ends are equal
     while (left >= 0 && right < length && str[left] == str[right]) {
         left--;
         right++;
     }
     
-    // Return the substring from the found palindrome
-    // (left+1) to (right-1) will give the palindrome substring
-    static char longest[1000];  // To store the largest palindrome found
-    memset(longest, 0, sizeof(longest));  // Clear the previous palindrome
-    
-    // Copy the longest palindrome found
-    int j = 0;
-    for (int i = left + 1; i < right; i++) {
-        longest[j++] = str[i];
+    // Update start index and maximum length if a larger palindrome is found
+    int len = right - left - 1;  // The length of the palindrome
+    if (len > *maxLen) {
+        *maxLen = len;
+        *start = left + 1;
     }
-    
-    return longest;
 }
 
 // Function to find the largest palindromic substring
-char *longestPalindrome(char str[]) {
+char* longestPalindrome(char str[]) {
     int length = strlen(str);
-    char *largestPalindrome = "";
-    
+    int start = 0;
+    int maxLen = 1;  // Minimum palindrome length is 1 (a single character)
+
     // Loop over each character in the string as a center
     for (int i = 0; i < length; i++) {
         // Odd length palindromes (centered at i)
-        char *oddPalindrome = expandAroundCenter(str, i, i, length);
+        expandAroundCenter(str, i, i, length, &start, &maxLen);
         
         // Even length palindromes (centered between i and i+1)
-        char *evenPalindrome = expandAroundCenter(str, i, i + 1, length);
-        
-        // Compare the lengths of palindromes and update the largest one found
-        if (strlen(oddPalindrome) > strlen(largestPalindrome)) {
-            largestPalindrome = oddPalindrome;
-        }
-        if (strlen(evenPalindrome) > strlen(largestPalindrome)) {
-            largestPalindrome = evenPalindrome;
-        }
+        expandAroundCenter(str, i, i + 1, length, &start, &maxLen);
     }
+
+    // Create a new string to store the largest palindrome found
+    static char largest[1000];  // Buffer to store the longest palindrome substring
+    memset(largest, 0, sizeof(largest));  // Clear previous data
     
-    return largestPalindrome;
+    // Copy the longest palindrome to the result array
+    for (int i = 0; i < maxLen; i++) {
+        largest[i] = str[start + i];
+    }
+
+    return largest;
 }
 
 int main() {
     char str[1000];
     
     // Read input string
-    scanf("%s",&str);
+    scanf("%s", str);
     
     // Find and print the largest palindromic substring
-    char *largestPal = longestPalindrome(str);
+    char* largestPal = longestPalindrome(str);
     printf("%s", largestPal);
     
     return 0;
