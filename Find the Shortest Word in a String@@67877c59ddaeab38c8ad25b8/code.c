ad39243c1ed2853155a3fd7f750b2e5ff6a3a041
Palak Manchanda <apalak910@gmail.com>
2025-03-20 00:45:11 +0530

    Solution to Find the Shortest Word in a String

--- Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
+++ Find the Shortest Word in a String@@67877c59ddaeab38c8ad25b8/code.c
@@ -37,5 +37,10 @@ int main() {
             printf("%c", a[i]);
         }
     }
+    else if(a[i]!=' '){
+        for(i=0;a[i]!='\0';i++){
+            printf("%c", a[i]);
+        }
+    }
     return 0;
 }
