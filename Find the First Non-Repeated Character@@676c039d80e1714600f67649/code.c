d73f7a4075c77cae7b4d1ecd283583d0e5bdc31d
Palak Manchanda <apalak910@gmail.com>
2025-03-19 11:33:24 +0530

    Solution to Find the First Non-Repeated Character

--- Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
+++ Find the First Non-Repeated Character@@676c039d80e1714600f67649/code.c
@@ -3,8 +3,9 @@
 int main(){
     char a[100];
     fgets(a, sizeof(a), stdin);
-    int i,j,count=1;
+    int i,j,count;
     for(i=0;a[i]!='\0';i++){
+        count=1;
         for(j=1;a[j]!='\0';j++){
             if(a[i]==a[j]){
                 count++;
