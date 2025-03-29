880043f00470bf76df9f6a88f5665c393dc723d3
Palak Manchanda <apalak910@gmail.com>
2025-03-29 20:20:43 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,11 +1,10 @@
 void fibonacciSeries(int n){
     static int n1=0,n2=1,n3;
-    printf("%d %d",n1,n2);
     if(n>0){
         n3=n1+n2;
         n1=n2;
         n2=n3;
         printf("%d ",n3);
-        fibonacciSeries(n-1);
+        fibonacciSeries(n-3);
     }
 }
