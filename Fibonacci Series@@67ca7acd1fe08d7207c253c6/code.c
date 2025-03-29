1364afc7a12375a4beaa717690102f9036bd1ebd
Palak Manchanda <apalak910@gmail.com>
2025-03-29 20:11:58 +0530

    Solution to Fibonacci Series

---
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -0,0 +1,12 @@
+int fibonacciSeries(int n){
+    while(n>0){
+        int a[n];
+        a[0]=0;
+        a[1]=1;
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum=sum+a[i];
+        }
+    }
+    return a[n];
+}
