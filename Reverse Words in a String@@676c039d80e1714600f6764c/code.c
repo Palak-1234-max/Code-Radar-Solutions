edf7a4c95688aa8d9835fc28a1554424bd6e675c
Palak Manchanda <apalak910@gmail.com>
2025-04-09 14:58:03 +0530

    Solution to Reverse Words in a String

--- Reverse Words in a String@@676c039d80e1714600f6764c/code.c
+++ Reverse Words in a String@@676c039d80e1714600f6764c/code.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <string.h>
-int main(){
-    char a[50];
-    int start,end;
-    fgets(a, sizeof(a), stdin);
-    int len=strlen(a);
-    if(a[len-1]=='\n'){
-        a[len-1]='\0';
-    }
-    start=0;
-    for(int i=0;i<=len;i++){
-        if(a[i]==' ' || a[i]=='\0'){
-            end=i-1;
-            while(start<end){
-                char temp;
-                temp=a[start];
-                a[start]=a[end];
-                a[end]=temp;
+
+int main() {
+    char str[100];
+    int start, end, len, i;
+
+    // Get the input string
+    fgets(str, sizeof(str), stdin);
+    
+    // Remove the newline character if present (fgets adds it)
+    str[strcspn(str, "\n")] = '\0';
+
+    // Find the length of the string
+    len = strlen(str);
+
+    // Step 1: Reverse each word in the string
+    start = 0;
+    for (i = 0; i <= len; i++) {
+        // When a space or the end of the string is found, reverse the word
+        if (str[i] == ' ' || str[i] == '\0') {
+            end = i - 1;  // End of the word
+            // Reverse the word between start and end
+            while (start < end) {
+                char temp = str[start];
+                str[start] = str[end];
+                str[end] = temp;
                 start++;
                 end--;
             }
-            start=i+1;
+            start = i + 1;  // Move to the start of the next word
         }
     }
-    printf("%s\n",a);
+
+    // Print the final string with the words reversed
+    printf("%s", str);
+    
     return 0;
 }
